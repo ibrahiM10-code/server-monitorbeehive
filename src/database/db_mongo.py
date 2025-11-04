ebeacb7b2e34a1e98970f309f0b8ab487af1e5f4
ibrahiM10 <ibrahim.sma1206@gmail.com>
2025-11-03 21:04:52 -0300

    Modificación en el update_datos_sensores de db_mongo.

--- src/database/db_mongo.py
+++ src/database/db_mongo.py
@@ -141,7 +141,10 @@ def get_datos_sensores(colmena_id):
 # Actualiza los datos de sensores de una colmena.
 def update_datos_sensores(colmena_id, update_fields: dict): 
     coleccion = db["sensores"]
-    update_fields["fecha"] = datetime.strptime(update_fields["fecha"], "%d-%m-%Y")
+    santiago_tz = pytz.timezone("America/Santiago")
+    fecha_dt = datetime.strptime(update_fields["fecha"], "%d-%m-%Y")
+    fecha_aware = santiago_tz.localize(fecha_dt)
+    update_fields["fecha"] = fecha_aware.astimezone(pytz.UTC)
     resultado = coleccion.update_many({"colmena_id": colmena_id}, {"$set": update_fields})
     return resultado.modified_count
 
--- src/helpers/generar_alerta.py
+++ src/helpers/generar_alerta.py
@@ -1,7 +1,7 @@
 from src.helpers.contenido_alerta import contenido_alerta
 from src.database.db_mongo import add_alerta
 from src.helpers.sendPushNotification import send_push_notification
-
+from datetime import datetime
 def generar_alerta(datos_sensores, colmena_id, userId, expo_push_token=""):
     alerta_temp = evalua_temperatura(float(str(datos_sensores["temperatura"])), colmena_id, userId)
     alerta_hum = evalua_humedad(float(str(datos_sensores["humedad"])), colmena_id, userId)
@@ -12,31 +12,3 @@ def generar_alerta(datos_sensores, colmena_id, userId, expo_push_token=""):
             result = send_push_notification(expo_push_token, alerta["titulo"], alerta["descripcion"])
         return result
     return True
-    
-def evalua_temperatura(temperatura, colmena_id, userId):
-    if temperatura < 33:
-        alerta_temp = contenido_alerta["temperatura"][0]["temperatura_baja"]
-    elif temperatura > 35:
-        alerta_temp = contenido_alerta["temperatura"][1]["temperatura_alta"]
-    else:
-        alerta_temp = contenido_alerta["temperatura"][2]["temperatura_optima"]
-    add_alerta(alerta_temp, colmena_id, userId)
-    return alerta_temp
-
-def evalua_humedad(humedad, colmena_id, userId):
-    if humedad < 40:
-        alerta_hum = contenido_alerta["humedad"][0]["humedad_baja"]
-    elif humedad > 70:
-        alerta_hum = contenido_alerta["humedad"][1]["humedad_alta"]
-    else:
-        alerta_hum = contenido_alerta["humedad"][2]["humedad_optima"]
-    add_alerta(alerta_hum, colmena_id, userId)
-    return alerta_hum
-
-def evalua_peso(peso, colmena_id, userId):
-    if peso <= 0.3:
-        alerta_peso = contenido_alerta["peso"][0]["peso_bajo"]
-    else:
-        alerta_peso = contenido_alerta["peso"][1]["peso_optimo"]
-    add_alerta(alerta_peso, colmena_id, userId)
-    return alerta_peso
